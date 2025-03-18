6e8e172b94a78acf259cb552bac2c5aac99ae2e2
ronmar <sigma4726spring@gmail.com>
2025-03-17 22:21:16 -0600

    feature: componente Home creado

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Login from "../views/Login";
+import Home from "../views/Home";
 import Error404 from "../views/Error404";
 
 const router = createBrowserRouter([
@@ -8,7 +9,13 @@ const router = createBrowserRouter([
         path: '/login',
         element: <Login />,
         errorElement: <Error404 />,
+    },
+    {
+        path: '/',
+        element: <Home />,
+        errorElement: <Error404 />,
     }
+
 ]);
 
 const MyRoutes = () => <RouterProvider router={router}  />;
