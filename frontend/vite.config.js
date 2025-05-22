deef4de872c1ddd66a91b6ae4afcb120bbd5aa49
ronmar <sigma4726spring@gmail.com>
2025-05-21 22:41:45 -0600

    feature: validation for development and production mode to serve files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.VITE_NODE_ENV === "production" ? "./" : "/",
+  build: {
+    outDir: "dist",
+  },
+});
