9ac3bb7cea4ca2637ebeda3f498fde4846cbee8b
ronmar <sigma4726spring@gmail.com>
2025-05-22 21:28:18 -0600

    feature: Vite configuration modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_NODE_ENV === "production" ? "./" : "/",
+  base: "/", // Use "/" for Netlify and most static hosts
   build: {
     outDir: "dist",
   },
